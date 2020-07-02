201974c3201ef5a7ef60aa2103ee9a4538434e61
Sara Brea <Sara@MyMacs-MacBook-Pro.local>
2020-07-02 11:33:11 -0500

    added validation to urls in business model, also made draft associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,22 @@
 const db = require('./db')
+const Business = require('./models/business')
+const User = require('./models/user')
+const Tier = require('./models/tier')
+const Subscription = require('./models/subscription')
 
+User.belongsToMany(Business, {through: Subscription})
+Business.belongsToMany(User, {through: Subscription})
+Tier.belongsTo(Business)
+Business.hasMany(Tier)
+Subscription.belongsTo(Tier)
+Tier.hasOne(Subscription)
 // register models
 require('./models')
 
-module.exports = db
+module.exports = {
+  db,
+  Business,
+  Tier,
+  Subscription,
+  User
+}
