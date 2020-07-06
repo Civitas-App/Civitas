6e3d78d5ea84ef8049ebe1f1fe48f54a01ec5d56
Sara Brea <Sara@MyMacs-MBP.attlocal.net>
2020-07-06 14:18:56 -0500

    added some description to homepage

--- client/components/Utility/Homepage.js
+++ client/components/Utility/Homepage.js
@@ -4,6 +4,13 @@ export function Homepage() {
   return (
     <div>
       <h1>Welcome to Civitas</h1>
+      <h2>Who are we?</h2>
+      <h3>
+        Civitas is a subscription based website to support local businesses
+        affected by covid/hardships. Users can pledge a monthly "donation" to
+        their favorite local businesses in need and in exchange, businesses will
+        provide users with food or services based on tiers.
+      </h3>
     </div>
   )
 }
