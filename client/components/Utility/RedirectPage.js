559022c9c78a56c74c17296d98d2c0db35c36974
Jordan Raleigh <jraleigh1127@gmail.com>
2020-07-09 15:51:09 -0500

    fixed redux and linking

--- client/components/Utility/RedirectPage.js
+++ client/components/Utility/RedirectPage.js
@@ -1,35 +1,10 @@
 import React, {useEffect} from 'react'
-import store from '../../store'
-/* Middle-Page new Component
-On mount, query the db
-  - Are they an existing user? NO.
-    - if they dont update the user role with req.session.role
-    - direct front end to load proper role form
-  - Are they an existing user? Yes.
-    - Send back data for customer/business
-    - Tell front end to load the correct portal
-  // Conditional rendering
-  if (thisThing){
-    return <CustomerForm/>
-  }
-  else {
-    return <BusinessForm/>
-  }
-
-  // Redirect Component from react-router
-
-  <Redirect to='/some-path'/>
-}
-  // pushing to history
-  this.props.history.push('/new-path')
-
-  use redux.state.store user.role
-    */
+import {useSelector} from 'react-redux'
 
 const RedirectPage = props => {
+  const role = useSelector(state => state.user.role)
+  console.log('redirectPage being hit')
   useEffect(() => {
-    const reduxState = store.getState()
-    const {role} = reduxState.user
     console.log('reduxxxxxx', role)
     if (role === 'business') {
       props.history.push('/business/portal')
@@ -39,7 +14,7 @@ const RedirectPage = props => {
     } else {
       props.history.push('/user/signup')
     }
-  })
+  }, [])
 
   return <div>Redirecting to your portal</div>
 }
