8b0eb02482e3c67b15d3ddea6230bc6be03d697d
Jordan Raleigh <jraleigh1127@gmail.com>
2020-07-19 21:02:19 -0500

    sassy search businesses

--- client/components/SearchBar/AllBusinesses.js
+++ client/components/SearchBar/AllBusinesses.js
@@ -6,15 +6,15 @@ const AllBusinesses = () => {
   const businesses = useSelector(state => state.businesses)
 
   return (
-    <div id="all_businesses">
+    <div id="all_businesses_search">
       {businesses.map(query => (
         <div id="business_card" key={query.id}>
           <NavLink className="navlink" to={`/business/${query.id}`}>
-            <h4>Name: {query.name}</h4>
+            <h2>{query.name}</h2>
           </NavLink>
           <img id="business_card_img" src={query.headerPhoto} />
           <div className="description">
-            <h4>Description: {query.description}</h4>
+            <h4>{query.description}</h4>
           </div>
           <div />
         </div>
